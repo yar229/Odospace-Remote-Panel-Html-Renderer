8079b3cb4ba7c820b32b2b45d340c3376784cb50
YaR <yar229@yandex.ru>
2025-02-07 11:22:36 +0300

    initial

---
+++ ConsoleApp26/Aida/AidaRDsp.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace YaR.Odospace.RemotePanel.HtmlRendererClient.Aida;
+
+public static class AidaRDsp
+{
+    // send_image(USHORT usPort, char * pszAddress, ULONG ulImageId, void * pImage, ULONG ulImageSize)
+    [DllImport("aidardsp.dll", EntryPoint = "send_image")]
+    public static extern int SendImage(ushort port, nint address, uint imageId, nint imageContent, uint imageSize);
+}
---
+++ ConsoleApp26/Logger.cs
@@ -0,0 +1,14 @@
+﻿namespace YaR.Odospace.RemotePanel.HtmlRendererClient;
+
+public static class Logger
+{
+    public static void Info(string msg)
+    {
+        Console.WriteLine($"{DateTime.Now}\t{msg}");
+    }
+
+    public static void Error(string msg, Exception exception)
+    {
+        Console.WriteLine($"{DateTime.Now}\t{msg}\t{exception.Message}");
+    }
+}
